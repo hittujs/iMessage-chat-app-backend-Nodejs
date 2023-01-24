11218bb7e9ae4ee0b561c54d0b0f2c492f31a799
Hitesh Chhatvani <hiteshchhatvani@gmail.com>
2023-01-24 22:45:32 +0530

    feat: make search working in backend

--- src/graphql/resolvers/user.ts
+++ src/graphql/resolvers/user.ts
@@ -1,8 +1,43 @@
+import { User } from "@prisma/client";
+import { ApolloError } from "apollo-server-core";
 import { CreateUsernameResponse, GraphQLContext } from "../../util/types";
 
 export const resolvers = {
   Query: {
-    searchUsers: () => {},
+    searchUsers: async (
+      _: any,
+      args: { username: string },
+      context: GraphQLContext
+    ): Promise<Array<User>> => {
+      const { username: searchedUsername } = args;
+      const { session, prisma } = context;
+
+      if (!session?.user) {
+        throw new ApolloError("Not authorized");
+      }
+
+      const {
+        user: { username: myUsername },
+      } = session;
+
+      try {
+        const users = await prisma.user.findMany({
+          where: {
+            username: {
+              contains: searchedUsername,
+              not: myUsername,
+              mode: "insensitive",
+            },
+          },
+        });
+        console.log(users, "users 12333");
+
+        return users;
+      } catch (error: any) {
+        console.log("searchusers error", error);
+        throw new ApolloError(error?.message);
+      }
+    },
   },
   Mutation: {
     createUsername: async (
